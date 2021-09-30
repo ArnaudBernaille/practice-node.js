211809dae2cc28e6f19baa53de7259cfad5094de
ArnaudBernaille <arbernaille2@gmail.com>
2021-09-30 20:28:18 +0200

    simple node exemple

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => x*y
+};
+
+function solRect(l,b){
+    console.log("solving rectangle with l = " + l + " and b = " + b);
+    if (l < 0 || b < 0) {
+        console.log("les dimensions du rectangles doivent etre supérieur à 0");
+    }
+
+    else{
+        console.log("the area of the rectangle is " + rect.area(l,b));
+        console.log("the perimeter of the rectangle is " + rect.perimeter(l,b));
+    }
+}
+
+solRect(2,4);
